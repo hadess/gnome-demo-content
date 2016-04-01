ffc612e73e931cc29c76ef1c1aebdcd7cb605eeb
Bastien Nocera <hadess@hadess.net>
2016-04-01 21:23:21 +0200

    Check for MP3 support in GStreamer
    It's needed to handle the music

--- check-tools.sh
+++ check-tools.sh
@@ -5,6 +5,12 @@ check()
 	which $1 > /dev/null 2>&1 || (echo "$1 not found" ; exit 1)
 }
 
+check_gst()
+{
+	gst-inspect-1.0 $1 > /dev/null 2>&1 || (echo "GStreamer plugin $1 not found" ; exit 1)
+}
+
 check nm-online
 check youtube-dl
 check ffmpeg
+check_gst mad
