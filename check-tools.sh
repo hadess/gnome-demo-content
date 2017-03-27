4f9ae2210b02d201612a5f21d4ae107f3fced175
Bastien Nocera <hadess@hadess.net>
2017-03-27 17:12:28 +0200

    Check for mpg123 in addition to mad GStreamer plugin

--- check-tools.sh
+++ check-tools.sh
@@ -7,11 +7,15 @@ check()
 
 check_gst()
 {
-	gst-inspect-1.0 $1 > /dev/null 2>&1 || (echo "*** GStreamer plugin $1 not found" ; exit 1)
+	if test x$2 != x ; then
+		gst-inspect-1.0 $1 > /dev/null 2>&1 || gst-inspect-1.0 $2 || (echo "*** GStreamer plugins $1 and $2 not found" ; exit 1)
+	else
+		gst-inspect-1.0 $1 > /dev/null 2>&1 || (echo "*** GStreamer plugin $1 not found" ; exit 1)
+	fi
 }
 
 check nm-online
 check youtube-dl
 check ffmpeg
 check git-lfs
-check_gst mad
+check_gst mad mpg123
